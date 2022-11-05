eacd7339d8d4429dc49c0620b33641e6e8c2ddd3
JHIH-LEI <vicky88888@gmail.com>
2022-11-06 01:31:04 +0800

    update new user router

--- users/src/routes/new.ts
+++ users/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/user";
 import jsonwebtoken from "jsonwebtoken";
+import { validateRequest } from "@domosideproject/twitter-common";
 const router = express.Router();
 
 type PostUserRequest = {
@@ -19,13 +20,14 @@ router.post(
     body("email").isEmail().withMessage("Email must valid"),
     body("account")
       .trim()
-      .isLength({ min: 6, max: 50 })
-      .withMessage("account must between 6~50 characters"),
+      .isLength({ min: 4 })
+      .withMessage("account must at least 4 characters"),
     body("password")
       .trim()
       .isLength({ min: 6, max: 50 })
       .withMessage("password must between 6~50 characters"),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const { name, email, account, password, checkPassword } =
       req.body as PostUserRequest;
