52401bd3e9407706dde1f74f57df7e7707b157b5
JHIH-LEI <vicky88888@gmail.com>
2022-11-05 16:35:22 +0800

    app and test setup

---
+++ users/src/app.ts
@@ -0,0 +1,15 @@
+import { json } from "body-parser";
+import express from "express";
+import cookieSession from "cookie-session";
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // cookie內容不需加密，因為jwt已經加密了
+    secure: process.env.NODE_ENV !== "test", // https才會set-cookie(如果有在req.session物件放東西)
+  })
+);
+
+export { app };
