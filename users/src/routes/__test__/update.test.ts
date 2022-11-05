bfd967c2e7499cccae73d6651e89c5c235aa0a75
JHIH-LEI <vicky88888@gmail.com>
2022-11-06 01:28:24 +0800

    update update user test

--- users/src/routes/__test__/update.test.ts
+++ users/src/routes/__test__/update.test.ts
@@ -19,6 +19,7 @@ it("update name and intro successful", async () => {
 
   await request(app)
     .put(`/api/users/${newUser!.id}`)
+    .set("Cookie", global.getCookie(newUser?.id))
     .send({ name: "updated", intro: "updated" })
     .expect(204);
 
@@ -27,4 +28,23 @@ it("update name and intro successful", async () => {
   expect(updatedUser!.name).toBe("updated");
 
   expect(updatedUser!.intro).toBe("updated");
+});
+
+it("return 401 for trying to modify other file", async () => {
+  const user = User.build({
+    name: "test",
+    email: "test@email.com",
+    password: "12345662341",
+    account: "user1",
+  });
+  await user.save();
+
+  const newUser = await User.findOne({ email: "test@email.com" });
+
+  expect(newUser).not.toBeNull();
+
+  await request(app)
+    .put(`/api/users/${newUser!.id}`)
+    .send({ name: "updated", intro: "updated" })
+    .expect(401);
 });
