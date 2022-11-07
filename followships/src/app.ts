2de3758f8b895c75a03bf04e9d615cde791896f8
JHIH-LEI <vicky88888@gmail.com>
2022-11-08 01:18:39 +0800

    followship server init

---
+++ followships/src/app.ts
@@ -0,0 +1,29 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: `${process.cwd()}/src` });
+import { json } from "body-parser";
+import express from "express";
+import cookieSession from "cookie-session";
+import { unFollowRouter } from "./routes/delete";
+import { followRouter } from "./routes/new";
+import { topUserRouter } from "./routes/topUser";
+import { userFollowings } from "./routes/userFollowings";
+import { userFollowers } from "./routes/userFollowers";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // cookie內容不需加密，因為jwt已經加密了
+    secure: process.env.NODE_ENV !== "test", // https才會set-cookie(如果有在req.session物件放東西)
+  })
+);
+
+app.use("/api/followships", topUserRouter);
+app.use("/api/followships", userFollowers);
+app.use("/api/followships", userFollowings);
+app.use("/api/followships", followRouter);
+app.use("/api/followships", unFollowRouter);
+
+export { app };
