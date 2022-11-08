c3288438fb967bc35bfc152aac92a6a93956cd7a
JHIH-LEI <vicky88888@gmail.com>
2022-11-08 14:10:40 +0800

    add subscribeship test case

---
+++ subscribeships/src/routes/__test__/delete.test.ts
@@ -0,0 +1,33 @@
+import request from "supertest";
+import { app } from "../../app";
+import { db } from "../../models/index";
+const { User, Subscribeship } = db;
+
+jest.setTimeout(10000);
+test("unsubscribe", async () => {
+  const loginUser = await User.create({
+    id: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }).catch((err) => {
+    throw new Error(err);
+  });
+
+  const targetUser = await User.create({
+    id: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+
+  await Subscribeship.create({
+    subscriberId: loginUser.id,
+    subscribingId: targetUser.id,
+  });
+
+  return request(app)
+    .delete(`/api/subscribeships/${targetUser.id}`)
+    .set("Cookie", global.getCookie(loginUser.id))
+    .expect(204);
+});
+
+// TODO: 資料不存在不給新增/更新的話要回傳什麼？？
---
+++ subscribeships/src/routes/__test__/new.test.ts
@@ -0,0 +1,25 @@
+import request from "supertest";
+import { app } from "../../app";
+import { db } from "../../models/index";
+const { User } = db;
+jest.setTimeout(10000);
+it("start subscribe", async () => {
+  jest.setTimeout(10000);
+  const [loginUser, targetUser] = await Promise.all([
+    User.create({
+      id: 1,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+    User.create({
+      id: 2,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }),
+  ]);
+
+  return request(app)
+    .post(`/api/subscribeships/${targetUser.id}`)
+    .set("Cookie", global.getCookie(loginUser.id))
+    .expect(201);
+});
