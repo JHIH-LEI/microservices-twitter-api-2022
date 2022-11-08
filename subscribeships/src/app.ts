e23f3c2f7c8bdaebe9707f9928f13b11040ecc1c
JHIH-LEI <vicky88888@gmail.com>
2022-11-08 14:09:40 +0800

    subscribeship server init

---
+++ subscribeships/src/app.ts
@@ -0,0 +1,23 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: `${process.cwd()}/src/.env` });
+import { json } from "body-parser";
+import express from "express";
+import cookieSession from "cookie-session";
+import { unSubscribeRouter } from "./routes/delete";
+import { subscribeRouter } from "./routes/new";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // cookie內容不需加密，因為jwt已經加密了
+    secure: process.env.NODE_ENV !== "test", // https才會set-cookie(如果有在req.session物件放東西)
+  })
+);
+
+app.use("/api/subscribeships", subscribeRouter);
+app.use("/api/subscribeships", unSubscribeRouter);
+
+export { app };
