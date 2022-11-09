3ab28cf0313bfaf47b91e3786cd5b044aea1bb11
JHIH-LEI <vicky88888@gmail.com>
2022-11-09 22:35:40 +0800

    tweets server init

---
+++ tweets/src/app.ts
@@ -0,0 +1,35 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: `${process.cwd()}/src/.env` });
+import { json } from "body-parser";
+import express from "express";
+import cookieSession from "cookie-session";
+import { showTweetRouter } from "./routes/show";
+import { errorHandler, NotFoundError } from "@domosideproject/twitter-common";
+import { updateTweetRouter } from "./routes/update";
+import { deleteTweetRouter } from "./routes/delete";
+import { newTweetRouter } from "./routes/new";
+import { indexTweetRouter } from "./routes/index";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // cookie內容不需加密，因為jwt已經加密了
+    secure: process.env.NODE_ENV !== "test", // https才會set-cookie(如果有在req.session物件放東西)
+  })
+);
+
+app.use("/api/tweets", showTweetRouter);
+app.use("/api/tweets", updateTweetRouter);
+app.use("/api/tweets", deleteTweetRouter);
+app.use("/api/tweets", newTweetRouter);
+app.use("/api/tweets", indexTweetRouter);
+
+app.all("*", () => {
+  throw new NotFoundError("can not find route");
+});
+
+app.use(errorHandler);
+export { app };
