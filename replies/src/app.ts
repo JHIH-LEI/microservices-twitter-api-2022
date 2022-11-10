b4b18a8fc4ec13046f4cd2815753a9bef03b4d98
JHIH-LEI <vicky88888@gmail.com>
2022-11-10 18:50:58 +0800

    reply server init

---
+++ replies/src/app.ts
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: `${process.cwd()}/src/.env` });
+import { json } from "body-parser";
+import express from "express";
+import cookieSession from "cookie-session";
+import { getTweetRepliesRouter } from "./routes/getTweetReplies";
+import { getUserRepliesRouter } from "./routes/getUserReplies";
+import { newReplyRouter } from "./routes/new";
+import { deleteReplyRouter } from "./routes/delete";
+import { errorHandler, NotFoundError } from "@domosideproject/twitter-common";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // cookie內容不需加密，因為jwt已經加密了
+    secure: process.env.NODE_ENV !== "test", // https才會set-cookie(如果有在req.session物件放東西)
+  })
+);
+
+app.use("/api/replies", getTweetRepliesRouter);
+app.use("/api/replies", getUserRepliesRouter);
+app.use("/api/replies", newReplyRouter);
+app.use("/api/replies", deleteReplyRouter);
+
+app.all("*", () => {
+  throw new NotFoundError("can not find route");
+});
+
+app.use(errorHandler);
+export { app };
