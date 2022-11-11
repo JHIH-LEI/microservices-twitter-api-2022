6dbf84bf31859292c681402d4af308f17177656f
JHIH-LEI <vicky88888@gmail.com>
2022-11-12 01:45:13 +0800

    add comment to NotificationContent type

--- common/src/event/types/notification.ts
+++ common/src/event/types/notification.ts
@@ -6,7 +6,13 @@ export enum NotificationType {
   Reply = "reply",
 }
 
-export interface NotificationMessage {
+/**
+ * @param id 資源的id，像是新推文/新留言就會是給推文的id，追蹤/訂閱給觸發者的id
+ * @param createdAt 資源新增的時間
+ * @param main: 通知要顯示的內文，可能是空白(追蹤/訂閱)、新推文內文、新回覆的內容
+ */
+
+export interface NotificationCreatedContent {
   id: string;
   createdAt: string;
   type: NotificationType;
