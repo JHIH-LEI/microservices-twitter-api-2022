c16d3821344100153cc069dffc8f645dbce50ebf
JHIH-LEI <vicky88888@gmail.com>
2022-11-13 03:44:43 +0800

    update tweet-created-event properties

--- common/src/event/tweet-created-event.ts
+++ common/src/event/tweet-created-event.ts
@@ -1,11 +1,14 @@
 import { Queue } from "./queue";
 
 export interface TweetCreatedContent {
-  tweetId: string;
-  description: string;
+  id: string;
   userId: string;
+  description: string;
   name: string;
   avatar: string;
+  createdAt: string;
+  updatedAt: string;
+  version: number;
 }
 
 export interface TweetCreatedEvent {
