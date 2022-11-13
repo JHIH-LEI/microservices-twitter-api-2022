f0bd7cbea72454824a9c8262dcbe2b4755bec4e1
JHIH-LEI <vicky88888@gmail.com>
2022-11-14 02:28:03 +0800

    feat: update like schema prevent multiple like on same tweet by same user

--- likes/src/models/like.ts
+++ likes/src/models/like.ts
@@ -1,14 +1,17 @@
+import { ConflictError } from "@domosideproject/twitter-common";
 import mongoose from "mongoose";
 import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface LikeAttrs {
-  tweetId: mongoose.Schema.Types.ObjectId;
-  userId: mongoose.Schema.Types.ObjectId;
+  tweetId: mongoose.Types.ObjectId;
+  userId: mongoose.Types.ObjectId;
 }
 
 interface LikeDoc extends mongoose.Document {
-  tweetId: mongoose.Schema.Types.ObjectId;
-  userId: mongoose.Schema.Types.ObjectId;
+  tweetId: mongoose.Types.ObjectId;
+  userId: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 interface LikeModel extends mongoose.Model<LikeDoc> {
@@ -41,6 +44,17 @@ const likeSchema = new mongoose.Schema(
 likeSchema.set("versionKey", "version");
 
 likeSchema.plugin(updateIfCurrentPlugin);
+
+// handle duplicate key error(multiple like by same user on same tweet)
+likeSchema.post("save", function (error: any, doc: LikeDoc, next: any) {
+  if (error.code === 11000) {
+    next(new ConflictError("can not like same tweet more than once"));
+  }
+  next();
+});
+
+// prevent tweet have multiple like from same user
+likeSchema.index({ tweetId: 1, userId: 1 }, { unique: true });
 
 likeSchema.statics.build = (attrs: LikeAttrs) => {
   return new Like(attrs);
