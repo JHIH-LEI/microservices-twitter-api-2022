bdad4e2623c7b13ac6c178be963f090ab2e6444e
JHIH-LEI <vicky88888@gmail.com>
2022-11-24 16:39:44 +0800

    change rabbitmq to direct exchange in followships

--- followships/src/publishers/followship-created.ts
+++ followships/src/publishers/followship-created.ts
@@ -1,11 +1,12 @@
 import {
+  BindingKey,
   FollowshipCreatedEvent,
   Publisher,
-  Queue,
 } from "@domosideproject/twitter-common";
 import { senderChannel } from "../app";
 
 export class FollowshipCreatedPublisher extends Publisher<FollowshipCreatedEvent> {
-  readonly queue = Queue.FollowshipCreated;
+  readonly routingKey: BindingKey = BindingKey.FollowshipCreated;
   readonly channel = senderChannel;
+  readonly durable: boolean = true;
 }
--- followships/src/subscribeships/user-updated.ts
+++ followships/src/subscribeships/user-updated.ts
@@ -1,6 +1,8 @@
 import {
+  BindingKey,
+  getQueueName,
   Listener,
-  Queue,
+  Service,
   UserUpdatedEvent,
 } from "@domosideproject/twitter-common";
 import { Message } from "amqplib";
@@ -8,8 +10,10 @@ import { listenerChannel } from "../app";
 import { db } from "../models";
 
 export class UserUpdatedConsumer extends Listener<UserUpdatedEvent> {
-  readonly queue = Queue.UserUpdated;
+  readonly queue = getQueueName(Service.User, this.bindingKey);
   readonly channel = listenerChannel;
+  readonly durable: boolean = true;
+  readonly bindingKey: BindingKey = BindingKey.UserUpdated;
 
   async consumeCallBack(
     content: UserUpdatedEvent["content"],
